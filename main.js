c09864a4a896f03599c5e7ea4a622c4e3ee853d5
kutlo2019 <kutlopopo@rocketmail.com>
2021-08-18 10:48:34 +0200

    allow user to traverse a provided maize

--- main.js
+++ main.js
@@ -63,15 +63,24 @@ function updateField() {
                 let downChecker = location[0] + 1; // A downward movement increases the location[0] attribute
                 if (downChecker >= myField.length) { // Check if user CANNOT move down
                     // User NOT allowed to move down
-                    console.log('You are out of bounds!!')
+                    console.log('You are out of bounds. \n!!GAME OVER!!')
+                    gamePlay = false;
                 } else {
                     // User allowed to move down. Swap the path square and field square
-                    const temp = myField[location[0]][location[1]];
-                    myField[location[0]][location[1]] = myField[location[0] + 1][location[1]];
-                    myField[location[0] + 1][location[1]] = temp;
-                    updateLocation(move);
-                    aField.field = myField;
-                    aField.print();
+                    const temp = myField[location[0] + 1][location[1]];
+                    if (temp === hole) {
+                        console.log('You fell in a hole. \n!!GAME OVER!!');
+                        gamePlay = false;
+                    } else if ( temp === fieldCharacter) {
+                        myField[location[0] + 1][location[1]] = myField[location[0]][location[1]];
+                        myField[location[0]][location[1]] = temp;
+                        updateLocation(move);
+                        aField.field = myField;
+                        aField.print();
+                    } else {
+                        console.log('Congratulations! <3 You have found your hat!')
+                        gamePlay = false;
+                    }
                 }
                 break;
             case 'u':
@@ -79,15 +88,24 @@ function updateField() {
                 let upChecker = location[0] - 1; // A upward movement increases the location[0] attribute
                 if (upChecker < 0 ) { // Check if user CANNOT move up
                     // User NOT allowed to move up
-                    console.log('You are out of bounds!!')
+                    console.log('You are out of bounds. \n!!GAME OVER!!');
+                    gamePlay = false;
                 } else {
                     // User allowed to move up. Swap the path square and field square
-                    const temp = myField[location[0]][location[1]];
-                    myField[location[0]][location[1]] = myField[location[0] - 1][location[1]];
-                    myField[location[0] - 1][location[1]] = temp;
-                    updateLocation(move);
-                    aField.field = myField;
-                    aField.print();
+                    const temp = myField[location[0] - 1][location[1]];
+                    if (temp === hole) {
+                        console.log('You fell in a hole. \n!!GAME OVER!!');
+                        gamePlay = false;
+                    } else if (temp === fieldCharacter) {
+                        myField[location[0] - 1][location[1]] = myField[location[0]][location[1]];
+                        myField[location[0]][location[1]] = temp;
+                        updateLocation(move);
+                        aField.field = myField;
+                        aField.print();
+                    } else {
+                        console.log('Congratulations! <3 You have found your hat!')
+                        gamePlay = false;
+                    }
                 }
                 break;
             case 'l':
@@ -95,15 +113,24 @@ function updateField() {
                  let leftChecker = location[1] - 1; // A left movement decreases the location[1] attribute
                  if (leftChecker < 0 ) { // Check if user CANNOT move left
                      // User NOT allowed to move left
-                     console.log('You are out of bounds!!')
+                     console.log('You are out of bounds. \n!!GAME OVER!!');
+                     gamePlay = false;
                  } else {
                      // User allowed to move left. Swap the path square and field square
-                     const temp = myField[location[0]][location[1]];
-                     myField[location[0]][location[1]] = myField[location[0]][location[1] - 1];
-                     myField[location[0]][location[1] - 1] = temp;
-                     updateLocation(move);
-                     aField.field = myField;
-                     aField.print();
+                     const temp = myField[location[0]][location[1] - 1];
+                     if (temp === hole) {
+                        console.log('You fell in a hole. \n!!GAME OVER!!');
+                        gamePlay = false;
+                    } else if (temp === fieldCharacter) {
+                        myField[location[0]][location[1] - 1] = myField[location[0]][location[1]];
+                        myField[location[0]][location[1]] = temp;
+                        updateLocation(move);
+                        aField.field = myField;
+                        aField.print();
+                    } else {
+                        console.log('Congratulations! <3 You have found your hat!')
+                        gamePlay = false;
+                    }
                  }
                 break;
             case 'r':
@@ -111,21 +138,29 @@ function updateField() {
                  let rightChecker = location[1] + 1; // A right movement increases the location[1] attribute
                  if (rightChecker >= myField[0].length ) { // Check if user CANNOT move right
                      // User NOT allowed to move right
-                     console.log('You are out of bounds!!')
+                     console.log('You are out of bounds. \n!!GAME OVER!!')
+                     gamePlay = false;
                  } else {
                      // User allowed to move right. Swap the path square and field square
-                     const temp = myField[location[0]][location[1]];
-                     myField[location[0]][location[1]] = myField[location[0]][location[1] + 1];
-                     myField[location[0]][location[1] + 1] = temp;
-                     updateLocation(move);
-                     aField.field = myField;
-                     aField.print();
+                     const temp = myField[location[0]][location[1] + 1];
+                     if (temp === hole) {
+                        console.log('You fell in a hole. \n!!GAME OVER!!');
+                        gamePlay = false;
+                    } else if (temp === fieldCharacter) {
+                        myField[location[0]][location[1] + 1] = myField[location[0]][location[1]];
+                        myField[location[0]][location[1]] = temp;
+                        updateLocation(move);
+                        aField.field = myField;
+                        aField.print();
+                    } else {
+                        console.log('Congratulations! <3 You have found your hat!')
+                        gamePlay = false;
+                    }
                  }
                 break;
             default:
                 // Do something
                 console.log('Please enter a valide move. See instructions at the top.');
-                // move = prompt('make a move: \n');
                 break;
         }
     }
